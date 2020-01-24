52cba1f5c1d1b4efa33e950746f8e52797155df3
jdab2020 <jdabalos90@gmail.com>
2020-01-23 17:27:27 -0800

    reorganized / removed console logs

--- script.js
+++ script.js
@@ -5,61 +5,49 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-// Create an array of characters to be used
+// Created an array of characters to be used
 let num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 let lowChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let upChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 let specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "/", ",", ".", "<", ">", ";", ":", "{", "}", "[", "]", "`", "~",];
 
-
-
 function generatePassword() {
-  // Asks how long of a password
-  let passwordLength = parseInt(prompt("Choose password length: 8-128"));
-    
-  // Checks if password length is within parameter
-  while (passwordLength < 8 || passwordLength > 128 || passwordLength === NaN || passwordLength === null) {
-    alert("Password length is either too short or too long!");
-    passwordLength = parseInt(prompt("Choose password length: 8-128"));
+  let confirmNum = false;
+  let confirmLowChar = false;
+  let confirmUpChar = false;
+  let confirmSpecChar = false;
+  let passwordLength = 0;
+  // Loop checks if password length is within parameter
+  // But doesn't run again if no input or if cancelled 
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = parseInt(prompt("Choose the length of password between 8 - 128 characters"));
+    if (passwordLength < 8 || passwordLength > 128){
+      alert("Length of password is either too long or too short")
+    }
   }
-  // Checks only once if clicked no input/okay/cancel....
-
-  // Asks which characters to include in password
-  let confirmNum = confirm("Include numbers?");
-  let confirmLowChar = confirm("Include lower case alphabet?");
-  let confirmUpChar = confirm("Include upper case alphabet?");
-  let confirmSpecChar = confirm("Include some special characters?");
-
-  // Checks if a type of character was chosen
+  // Loop checks if a type of character was selected
   while (confirmNum === false && confirmLowChar === false && confirmUpChar === false && confirmSpecChar === false) {
-    alert("Must have at least one type of characters!");
+    alert("Choose at least one type of characters!");
     confirmNum = confirm("Include numbers?");
     confirmLowChar = confirm("Include lower case alphabet?");
     confirmUpChar = confirm("Include upper case alphabet?");
     confirmSpecChar = confirm("Include some special characters?");
   }
-
-  // Creates arrays for characters to be used
+  // Create arrays for characters to be used
   let charConfirmed = [confirmNum, confirmLowChar, confirmUpChar, confirmSpecChar]; //Array of boolean
-  console.log(charConfirmed.length);
   let chars = [num, lowChar, upChar, specChar]; //Array within an array
   let charIdx = []; 
   let charsToAnArray = [];
   for (let i = 0; i < charConfirmed.length; i++) { //Used to see which character type was confirmed as "true" then adds index to a charIdx
     if (charConfirmed[i]) {
       charIdx.push(i);
-      // console.log(charIdx);
       charsToAnArray = charsToAnArray.concat(chars[i]); //Puts selected character type into a single array / not array within an array
-      // console.log(charsToAnArray.length);
     }
   }
   // pw = chars[index=charIdx][index=random number] this makes sure the first few characters satisfy the selected types
@@ -67,11 +55,9 @@ function generatePassword() {
   for (let i = 0; i < charIdx.length; i++) { 
     pw += chars[charIdx[i]][Math.floor(Math.random() * chars[charIdx[i]].length)];
   }
-  // This then fills the password length user selected
+  // This then fills the password length recquirement user selected
   for (let i = pw.length; i <= passwordLength; i++) {
-    let randIdx = Math.floor(Math.random() * charsToAnArray.length);
-    pw += charsToAnArray[randIdx];
+    pw += charsToAnArray[Math.floor(Math.random() * charsToAnArray.length)];
   }
-  
   return pw;
 }
